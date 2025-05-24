107789db105fd29dab469798bbf9f4541f71c068
@kentonyxa <kentonyxa@sxi.com.ph>
2025-05-24 13:14:49 +0800

    Add next and try again button

---
+++ Sources/BlumeSwiftUI/BlumeNextButton.swift
@@ -0,0 +1,70 @@
+//
+//  BlumeMainTextField.swift
+//  BlumeSwiftUI
+//
+//  Created by Kent Onyx Arintok on 5/17/25.
+//
+
+import SwiftUI
+
+public struct BlumeNextButton: View {
+    
+    public var nextAction: () -> Void
+    public var tryAgainAction: () -> Void
+    public var showTryAgain: Bool
+    
+    public init(
+        showTryAgain: Bool = false,
+        tryAgainAction: @escaping () -> Void = {},
+        nextAction: @escaping () -> Void = {}
+    ) {
+        self.showTryAgain = showTryAgain
+        self.tryAgainAction = tryAgainAction
+        self.nextAction = nextAction
+    }
+    
+    public var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                if showTryAgain {
+                    Button(action: tryAgainAction) {
+                        Text("Try Again")
+                            .font(.aeonikLight(size: 18))
+                            .foregroundStyle(Color(hex: "#303030"))
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color(hex: "#F8F7F6"))
+                    .cornerRadius(50)
+                } else {
+                    Button(action: {}) {
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                }
+                
+                
+                Button(action: nextAction) {
+                    Text("Next")
+                        .font(.aeonikLight(size: 18))
+                        .foregroundStyle(Color(hex: "#303030"))
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(hex: "#C0C0C0").opacity(0.5))
+                .cornerRadius(50)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 100)
+        }
+    }
+}
+
+#Preview {
+    BlumeNextButton(showTryAgain: false,
+                    tryAgainAction: { /*try again action*/ },
+                    nextAction: { /*next action*/ }
+    )
+}
